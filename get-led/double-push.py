f45df989a325aad02d41bedd5023f9c00c10eb03
StanislavKukushkin1 <owerdex@yandex.com>
2025-09-25 15:30:02 +0300

    Home tasks

---
+++ get-led/double-push.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+leds = [24, 22, 23, 27, 17, 25, 12, 16]
+
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)
+light_time = 0.2
+num = 0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+up = 9
+down = 10
+
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
+
+
+while True:
+    if GPIO.input(up) and not GPIO.input(down):
+        num = num + 1
+    elif GPIO.input(down) and not GPIO.input(up):
+        num = num - 1
+    if GPIO.input(up) and GPIO.input(down):
+        num = 255
+    dec = dec2bin(num)
+    dec.reverse()
+    for i in range(8):
+        if dec[i]:
+            GPIO.output(leds[i], 1)
+        else:
+            GPIO.output(leds[i], 0)
+    print(num)
+    time.sleep(light_time)
+
