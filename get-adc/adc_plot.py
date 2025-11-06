b89d5f42e901f019f09b82ba46cf41034d2cf321
Станислав <kukushkin.sa@phystech.edu>
2025-11-06 14:46:06 +0300

    asd

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -13,22 +13,31 @@ def plot_voltage_vs_time(time, voltage, max_voltage):
     plt.grid(True)
     plt.show()
 
+# def plot_sampling_period_hist(time):
+
+#     sampling_periods = []
+
+#     for i in range(1,len(time)):
+#         period = time[i] - time[i-1]
+#         sampling_periods.append(period)
+
+#     plt.figure(figsize=(10,6))
+#     plt.hist(sampling_periods)
+
+#     plt.title("Распределение периодов дискретиsзации измерений по времени на одно измерение")
+#     plt.ylabel('Количество измерений')
+#     plt.xlabel('Период измерения, с')
+#     plt.xlim(0,0.06)
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
+
+
 def plot_sampling_period_hist(time):
-
-    sampling_periods = []
-
-    for i in range(1,len(time)):
-        period = time[i] - time[i-1]
-        sampling_periods.append(period)
-
+    time.insert(0,0)
+    dTime = [time[i+1]-time[i] for i in range(len(time)-1)]
     plt.figure(figsize=(10,6))
-    plt.hist(sampling_periods)
-
-    plt.title("Распределение периодов дискретиsзации измерений по времени на одно измерение")
-    plt.ylabel('Количество измерений')
-    plt.xlabel('Период измерения, с')
-    plt.xlim(0,0.06)
-    plt.legend()
+    plt.hist(dTime,bins='auto',edgecolor = 'black')
     plt.grid(True)
     plt.show()
 
